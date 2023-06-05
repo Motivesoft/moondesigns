ba4a20eb1f7896ec3516ae7574e5968bbdfdab10
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2023-06-05 13:15:45 +0200

    As scraped from the Internet Archive

---
+++ oasisdelmar/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Oasis del Mar holiday apartment Mojacar Almeria Spain</title>
+<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" /> 
+<link rel="stylesheet" type="text/css" href="oasisdelmar.css">
+
+</head>
+
+<body>
+
+<div class="banner">
+<span class="view"><img src="view.jpg" alt="view"></span>
+<span class="view2"><img src="view2.jpg" alt="view"></span>
+<span class="oasis"><img src="oasisimage.gif" alt="oasis del mar"></span>
+</div>
+
+<!--Horizontal Menu Bar-->
+<div class="center">
+<ul id="horizmenu">
+<li class="hmenu"><a href="index.php">HOME</a></li>
+<li class="hmenu"><a href="apartment.html">THE APARTMENT</a></li> 
+<li class="hmenu"><a href="mojacar.html">ABOUT MOJ&Aacute;CAR</a></li> 
+<li class="hmenu"><a href="booking.html">PRICES, AVAILABILITY &amp; BOOKING</a></li> 
+<li class="hmenu"><a href="useful.html">USEFUL INFORMATION</a></li>
+</ul>
+</div>
+<!--End Horizontal Menu Bar-->
+
+<br>
+<div class="center">
+<img src="complex.jpg" alt="complex">
+</div>
+
+<p>
+Oasis del Mar is a complex of 50 brand new apartments located in a quiet
+area of Moj&aacute;car, but still close to all the amenities of Moj&aacute;car Playa.
+Our apartment is situated on the upper level at the rear of the complex,
+enjoying spectacular views of the sea from the roof terrace and
+overlooking the pool and garden area.
+</p>
+
+<div class="center">
+<p class="hometitle">
+Moj&aacute;car Playa - Almer&iacute;a - Spain
+</p>
+</div>
+
+<div class="center">
+<p id="footer">
+website &#169;2006 <a href="http://www.moondesigns.co.uk"><span style="color:#ffffff;">moondesigns</span></a> 
+</p>
+  <p>
+    <a href="http://validator.w3.org/check?uri=referer"><img
+        src="http://www.w3.org/Icons/valid-html401"
+        alt="Valid HTML 4.01 Transitional" height="31" width="88"></a>
+  </p>
+</div>
+
+</body>
+</html>
